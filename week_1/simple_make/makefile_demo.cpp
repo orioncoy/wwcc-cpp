1694d10ef902294c422faa73b1a47a67a314baa1
orioncoy <calvinocoy@gmail.com>
2025-04-08 13:44:58 -0700

    Simple Makefile
    Week 1 - Code Assignment 4(5?)
    Make file works as intended and properly compiles the source

---
+++ week_1/simple_make/makefile_demo.cpp
@@ -0,0 +1,12 @@
+//-------------------------------+-------------+------------+
+// Week 1 - Coding Assignment #5 | Calvin Losh | 08-04-2025 |
+//-------------------------------+-------------+------------+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "This program was compiled using a makefile";
+    return 0;
+}
