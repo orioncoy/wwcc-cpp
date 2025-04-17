1351f7ee1288ca94ddd114591679eafbb8cf0572
orioncoy <calvinocoy@gmail.com>
2025-04-16 22:13:15 -0700

    week 3 init
    general commit for week 3 initialization

--- week2/assignment0-formatSpec.cpp
+++ week2/assignment0-formatSpec.cpp
@@ -3,7 +3,23 @@
 //-------------------------------+-------------+------------+
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
+    bool            var_bool{};
+    char            var_char{};
+    short int       var_short_int{};
+    int             var_int{};
+    long int        var_long_long{};
+    long long int   var_long_long_int{};
+    float           var_float{};
+    double          var_double{};
+    string          var_string{};
 
+    cout << ""
+         << "";
+    
+    return 0;
 }
