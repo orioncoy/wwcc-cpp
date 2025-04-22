fb24a57391a3af51e9e86c067da42051ace391ed
orioncoy <calvinocoy@gmail.com>
2025-04-21 21:04:32 -0700

    week 2 assignment 2
    works and builds properly

--- week2/assignment2-numGame.cpp
+++ week2/assignment2-numGame.cpp
@@ -1,9 +1,38 @@
 //-------------------------------+-------------+------------+
-// Week 2 - Coding Assignment #2 | Calvin Losh | 10-04-2025 |
+// Week 2 - Coding Assignment #2 | Calvin Losh | 21-04-2025 |
 //-------------------------------+-------------+------------+
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
 
 int main() {
+    srand(time(0));
 
+    int randNum{rand() % 100 + 1};
+    int guess{};
+    int attempts{1};
+    bool loop{true};
+
+    cout << "\nNumber Guessing Game!"
+         << "\nI am thinking of a number between 1 and 100... what could it be???\n> ";
+
+    do {
+        cin >> guess;
+        if (guess != randNum) {
+            if (guess > randNum) {cout << "\nToo high, ";}
+            if (guess < randNum) {cout << "\nToo low, ";}
+            cout << "Try again! (attempt: " << attempts << ")\n> ";
+            attempts++;
+            continue;
+        } 
+        else if (guess == randNum) {
+            cout << "\nCongrats! you guessed it!" << endl;
+            loop = false;
+        } 
+        else {cout << "huh?!?";}
+    } while(loop);
+    return 0;
 }
