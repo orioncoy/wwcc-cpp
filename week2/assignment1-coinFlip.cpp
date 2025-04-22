64a0e9884ee5e866730ee7023ed56260d9fe939c
orioncoy <calvinocoy@gmail.com>
2025-04-21 20:43:01 -0700

    assignment 1 week 2
    works and builds correctly

--- week2/assignment1-coinFlip.cpp
+++ week2/assignment1-coinFlip.cpp
@@ -1,9 +1,43 @@
 //-------------------------------+-------------+------------+
-// Week 2 - Coding Assignment #1 | Calvin Losh | 10-04-2025 |
+// Week 2 - Coding Assignment #1 | Calvin Losh | 21-04-2025 |
 //-------------------------------+-------------+------------+
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
 
 int main() {
+    srand(time(0));
 
+    int flips{};
+    int heads{};
+    int tails{};
+    double hp{};
+    double tp{};
+
+    cout << "\nWelcome to the coin flipper simulator!"
+         << "\nHow many coin flips would you like to simulate?"
+         << "\n> ";
+    cin >> flips;
+    cout << "\nSimulating...\n";
+
+    for (int i = 0; i < flips; i++) {
+        int j = rand() %2;
+        if (j == 0) {tails++;}
+        else if (j == 1) {heads++;}
+        else {cout << "huh?";}
+    }
+
+    hp = (heads * 10)/(flips * 0.1);
+    tp = (tails * 10)/(flips * 0.1);
+
+    //percentages are mostly accurate but the printf rounding messes with it a bit.
+    cout << "\nHeads: " << heads 
+         << " (" << printf("%.1f", hp) << "%)"
+         << "\nTales: " << tails
+         << " (" << printf("%.1f", tp) << "%)"; 
+
+    return 0;
 }
