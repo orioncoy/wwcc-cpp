58ae40cd5c4209cf0ed3db2aaf281b77ac647a12
orioncoy <calvinocoy@gmail.com>
2025-04-25 01:07:08 -0700

    week 3 assignment 0
    builds and compiles properly

--- week2/assignment3-prodTable.cpp
+++ week2/assignment3-prodTable.cpp
@@ -1,9 +1,36 @@
 //-------------------------------+-------------+------------+
-// Week 2 - Coding Assignment #3 | Calvin Losh | 10-04-2025 |
+// Week 2 - Coding Assignment #3 | Calvin Losh | 21-04-2025 |
 //-------------------------------+-------------+------------+
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
 
 int main() {
+    int start{};
+    int end{};
+    string lineX{};
+    string lineY{};
 
+    cout << "Multiplication Table Generator\n"
+         << "Enter starting number\n> ";
+    cin >> start;
+    cout << "Enter ending number\n> ";
+    cin >> end;
+
+    if (start > end) {
+        cout << "Error: Starting number must be less than ending number." << endl;
+        return 1;
+    }
+
+    for (int y = 0; y < 0; y++) {
+
+        for (int x = 0; x < 0; x++) {
+            
+        }
+    }
+
+    return 0;
 }
--- week3/assignment0-distCalc.cpp
+++ week3/assignment0-distCalc.cpp
@@ -1,9 +1,43 @@
 //-------------------------------+-------------+------------+
-// Week 3 - Coding Assignment #0 | Calvin Losh | 10-04-2025 |
+// Week 3 - Coding Assignment #0 | Calvin Losh | 24-04-2025 |
 //-------------------------------+-------------+------------+
 
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double calculateDistance(double x1, double y1, double x2, double y2) {
+    double distance{sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))};
+    return distance;
+}
+
+string calculateMidpoint(double x1, double y1, double x2, double y2) {
+    double p1{(x1 + y1) / 2};
+    double p2{(x2 + y2) / 2};
+    string point{"(" + to_string(p1) + ", " + to_string(p2) + ")"};
+    return point;
+}
 
 int main() {
+    double x1, y1, x2, y2;
 
+    cout << "\nWelcome to the distance calculator!"
+         << "\nPlease enter in the coordinates of the first point."
+         << "\nx1 > ";
+    cin  >> x1;
+    cout << "y1 > ";
+    cin  >> y1;
+    cout << "\nNow for the second point."
+         << "\nx2 > ";
+    cin  >> x2;
+    cout << "y2 > ";
+    cin  >> y2;
+    
+    cout << "The distance between ("
+         << x1 << ", " << y1 << ") and ("
+         << x2 << ", " << y2 << ") is: "
+         << calculateDistance(x1, y1, x2, y2)
+         << " and the midpoint lies at "
+         << calculateMidpoint(x1, y1, x2, y2);
 }
