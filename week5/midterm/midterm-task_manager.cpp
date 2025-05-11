66010979859b0a1f76335d1e482411d3f6bb55b1
orioncoy <calvinocoy@gmail.com>
2025-05-11 03:14:30 -0700

    mid term init

---
+++ week5/midterm/midterm-task_manager.cpp
@@ -0,0 +1,16 @@
+//------------------+-------------+------------+
+// Week 5 - Midterm | Calvin Losh | 11-05-2025 |
+//------------------+-------------+------------+
+// Project - Task Manager
+// TODO List
+// [ ] Add tasks with title, description, priority and due date
+// [ ] Mark tasks as complete/incomplete
+// [ ] Display tasks sorted by priority or due date
+// [ ] Filter tasks by status (pending/complete)
+// [ ] Generate productivity reports (tasks completed per week)
+
+#include <iostream>
+
+int main() {
+    
+}
